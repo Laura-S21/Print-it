c43ad79696c49f674e06b02c1f37ea552de131d1
Laura <laura.simonnet.pro@gmail.com>
2023-05-11 11:56:50 +0200

    :feat; (carroussel) add event listener and bullet points

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,51 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+//Je déclare les variables des flèches
+const rightArrow = document.querySelector(".arrow_right");
+const leftArrow = document.querySelector(".arrow_left");
+
+//J'écoute les événements sur la flèche de droite
+rightArrow.addEventListener("click", function() {
+	alert("it works !");
+});
+
+//J'écoute les événements sur la flèche de gauche
+leftArrow.addEventListener("click", function() {
+	
+	alert("it works !");
+});
+
+// Fonction pour créer les points
+function generateDots() {
+// Je boucle sur le tableau des slides pour avoir le même nbr de points que d'éléments
+	for (let i = 0; i < slides.length; i++) {
+		// Récupération de l'élément qui affiche tous les points
+		const allDots = document.querySelector(".dots");
+		// Création d'une balise pour un point
+		const dotElement = document.createElement("div");
+		dotElement.className = "dot";
+
+		//Pour passer le 1er point en mode "actif" = si je suis à l'index 0
+		if (i == 0) {
+			//J'ajoute à l'élément dot la classe dot_selected
+			dotElement.classList.add ("dot_selected");
+		}
+
+		// Je rattache l'élément créé (le point) à l'élément parent
+		allDots.appendChild(dotElement);
+	}
+}
+
+// J'appelle la fonction pour générer les points
+generateDots();
+
+
+const allSlides = slides[i];
+//Création d'une balise pour l'image
+const imageElement = document.createElement("img");
+imageElement.src = slides.image;
+//Création d'une balise pour le texte
+const textElement = document.createElement("p");
+textElement.innerText = slides.tagLine;
